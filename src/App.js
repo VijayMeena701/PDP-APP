b373642587ea93677e8e2774367c641ed478335e
“VijayMeena701” <govardhanmeena701@gmail.com>
2021-03-17 19:26:40 +0530

    Added Signin Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import SignIn from './pages/SignIn'
+import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
+import CssBaseline from "@material-ui/core/CssBaseline"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline/>
+      <Router>
+        <Header/>
+        <Switch>
+        <Route exac path="/signin" component={SignIn} />
+        </Switch>
+      </Router>
     </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import withStyles from '@material-ui/core/styles/withStyles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link} from 'react-router-dom'
+
+const styles = (theme) => ({
+    root: {
+        flexGrow: 1,
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+      },
+      title: {
+        flexGrow: 1,
+      },
+      btn:{
+          color: '#fff',
+          textDecoration: 'none'
+      }
+})
+
+function Header(props) {
+    const classes = props.classes
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                Homepage
+                </Typography>
+                <Link to="/signin" className={classes.btn}>
+                    <Button color="inherit" >Login</Button>
+                </Link>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+Header.propTypes = {
+    classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Header);
+
